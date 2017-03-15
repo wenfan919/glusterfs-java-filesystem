9a4485a2413d5a8896e557dd447adb088b63f79c
wenfan919 <wenfan919@hotmail.com>
2017-03-15 15:13:05 +0800

    test

---
+++ glusterfs-java-filesystem/src/test/java/com/peircean/glusterfs/GlusterTest.java
@@ -0,0 +1,14 @@
+package com.peircean.glusterfs;
+
+public class GlusterTest {
+
+	public GlusterTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
